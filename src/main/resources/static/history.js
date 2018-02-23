b9cbdba5738617a1adf7f3a3ef55dd92370f6d9a
AZJoan <joanpadams@gmail.com>
2018-02-23 15:22:46 -0700

    updated history html and history js files

---
+++ src/main/resources/static/history.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var allOders = $("#divHistory").children().toArray();
+    allOrders.shift();
+
+    for(var i = 0; i < allOrders.length; i++){
+        $(allOrders[i]);
+    }
+});
