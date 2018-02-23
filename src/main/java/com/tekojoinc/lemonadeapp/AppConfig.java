32a0e2969aff38560c8f9385f8f5895f72080d9b
Ted <ted.stark@gmail.com>
2018-02-23 13:10:09 -0700

    update #1

--- src/main/java/com/tekojoinc/lemonadeapp/AppConfig.java
+++ src/main/java/com/tekojoinc/lemonadeapp/AppConfig.java
@@ -1,4 +1,13 @@
 package com.tekojoinc.lemonadeapp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
 public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class,args);
+    }
 }
--- src/main/java/com/tekojoinc/lemonadeapp/controller/AppController.java
+++ src/main/java/com/tekojoinc/lemonadeapp/controller/AppController.java
@@ -1,4 +1,13 @@
 package com.tekojoinc.lemonadeapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AppController {
+    @RequestMapping("/")
+    public String indexRoute(){
+        return "index";
+    }
 }
+
