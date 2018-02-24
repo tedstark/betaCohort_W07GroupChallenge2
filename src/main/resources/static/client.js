61044f65451bd9b2262316d5280109c79c4c308a
Ted <ted.stark@gmail.com>
2018-02-24 09:11:28 -0700

    shopping pages updated

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -1,7 +1,8 @@
+var cstDrink=2.0;
 var qtyLemon=0;
 var cstLemon=0;
-var qtyPLemon=0;
-var cstPlemon=0;
+var qtyPink=0;
+var cstPink=0;
 var qtyLime=0;
 var cstLime=0;
 var totalQty= [];
@@ -12,5 +13,6 @@ $(document).ready(function(){
 
 //Start the application
 function init(){
-    // shoppingPage();
+    shoppingInit();
 }
+
---
+++ src/main/resources/static/shoppingPage.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+
+});
+
+function shoppingInit() {
+    updateTotals()
+}
+
+function updateTotals() {
+    $("#txtQtyLemon").text("Quantity in Cart: " + qtyLemon);
+    $("#txtCstLemon").text("Cost in Cart: $" + cstLemon);
+    $("#txtQtyLime").text("Quantity in Cart: " + qtyLime);
+    $("#txtCstLime").text("Cost in Cart: $" + cstLime);
+    $("#txtQtyPink").text("Quantity in Cart: " + qtyPink);
+    $("#txtCstPink").text("Cost in Cart: $" + cstPink);
+    $("#pageFeedback").text("");
+}
+
+function addLemonade() {
+    if (($("#inputLemon").val()) == "") {
+        $("#pageFeedback").text("Enter a Quantity!");
+    } else {
+        numInput = parseInt($("#inputLemon").val());
+        qtyLemon += numInput;
+        // document.getElementById("inputFilled").val().clear()
+        cstLemon = (qtyLemon*cstDrink).toFixed(2);
+        updateTotals()
+    }
+}
+function remLemonade() {
+    if (($("#inputLemon").val()) == "") {
+        $("#pageFeedback").text("Enter a Quantity!");
+    } else {
+        var numFilled = parseFloat($("#inputLemon").val());
+        if ((qtyLemon-numFilled) < 0) {
+            $("#pageFeedback").text("Not that many in your cart!")
+        } else {
+            qtyLemon -= numFilled;
+            // document.getElementById("txtQtyFilled").innerText = "Quantity in Cart: " + qtyFilled;
+            cstLemon=(qtyLemon*cstDrink).toFixed(2);
+            // document.getElementById("txtCstFilled").innerText = "Cost in Cart: $" + cstFilled;
+            updateTotals()
+        }
+    }
+}
+function addLime() {
+    if (($("#inputLime").val()) == "") {
+        $("#pageFeedback").text("Enter a Quantity!");
+    } else {
+        numInput = parseInt($("#inputLime").val());
+        qtyLime += numInput;
+        cstLime = (qtyLime*cstDrink).toFixed(2);
+        updateTotals()
+    }
+}
+function remLime() {
+    if (($("#inputLime").val()) == "") {
+        $("#pageFeedback").text("Enter a Quantity!");
+    } else {
+        var numFilled = parseFloat($("#inputLime").val());
+        if ((qtyLime-numFilled) < 0) {
+            $("#pageFeedback").text("Not that many in your cart!")
+        } else {
+            qtyLime -= numFilled;
+            cstLime=(qtyLime*cstDrink).toFixed(2);
+            updateTotals()
+        }
+    }
+}
+function addPink() {
+    if (($("#inputPink").val()) == "") {
+        $("#pageFeedback").text("Enter a Quantity!");
+    } else {
+        numInput = parseInt($("#inputPink").val());
+        qtyPink += numInput;
+        cstPink = (qtyPink*cstDrink).toFixed(2);
+        updateTotals()
+    }
+}
+function remPink() {
+    if (($("#inputPink").val()) == "") {
+        $("#pageFeedback").text("Enter a Quantity!");
+    } else {
+        var numFilled = parseFloat($("#inputPink").val());
+        if ((qtyPink-numFilled) < 0) {
+            $("#pageFeedback").text("Not that many in your cart!")
+        } else {
+            qtyPink -= numFilled;
+            cstPink=(qtyLime*cstDrink).toFixed(2);
+            updateTotals()
+        }
+    }
+}
