24860342dd939e9b2d84bbce66cea79c6f759152
AZJoan <joanpadams@gmail.com>
2018-02-26 20:01:39 -0700

    cleaned up appearance of history, confirm and summary pages

--- src/main/resources/static/confirmorder.js
+++ src/main/resources/static/confirmorder.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
-
+    confirmInit();
 });
 
+function confirmInit() {
+    updateTotals()
+}
+
+function updateTotals() {
+    var num1 = (2 * parseInt($("#txtQtyLemon").text()));
+    var num2 = (2 * parseInt($("#txtQtyLime").text()));
+    var num3 = (2 * parseInt($("#txtQtyPink").text()));
+
+    console.log("num1: " + num1 + "num2: " + num2 + "num3: " + num3);
+    $("#txtCstLemon").text("Cost: $" + num1);
+    $("#txtCstLime").text("Cost: $" + num2);
+    $("#txtCstPink").text("Cost: $" + num3);
+    $("#pageFeedback").text("Thank you for your order!");
+}
